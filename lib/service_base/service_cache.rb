4ecb1e8a0b3a202a4c7fc7e738650b2c7651f757
Rafał Bigaj <rafal.bigaj@gmail.com>
2014-07-24 10:18:44 +0200

    Refs #12889 Common cache configuration (Rails and services)
    # Solution::Cache::DefaultStore
    # ServiceBase::ServiceCache uses Rails.cache by default (if Rails present)
    # IssueCache -> IssueJsonCache refactoring

--- lib/service_base/service_cache.rb
+++ lib/service_base/service_cache.rb
@@ -1,30 +1,20 @@
+require 'active_support/cache/dalli_store'
+
 module ServiceBase
 	class ServiceCache
 		attr_reader :store
 
 		def self.create
-			require 'active_support/cache/dalli_store'
 			new.store
 		end
 
 		def initialize
-			if config[:host]
-				@store = ActiveSupport::Cache::DalliStore.new(config[:host])
+			if defined?(Rails)
+				@store = Rails.cache
 			else
 				@store = ActiveSupport::Cache::DalliStore.new
 			end
 		end
 
-		def config
-			config_from_file || {}
-		end
-
-		def config_from_file
-			path = File.join(Rails.root, 'config', 'memcache.yml')
-			if File.exist?(path)
-				return YAML.load_file(path).symbolize_keys
-			end
-			nil
-		end
 	end
 end
