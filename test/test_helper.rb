76093f203def4c2d86cfedff60166bd10d963989
krzysztof <krzysztof.kucharski@puzzleflow.com>
2014-07-25 14:03:30 +0200

    Profile passing to aqs using cache. refs #12840 [AQS] Make it possible to attach preflight profile to preflight process

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,11 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require 'rubygems'
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-require 'bundler'
-Bundler.setup
-
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require "rails/test_help"
-#require 'test/unit'
-#require 'active_support/test_case'
 
 #SOLUTION_NAME = 'basic' unless defined?(SOLUTION_NAME)
 #require File.expand_path('../../../plugins/common_tools/test/common_test_helper', File.dirname(__FILE__))
-
-$: << File.expand_path('../../lib')
-require 'service_base'
 
 ServiceBase.eager_load_service_messages
 ServiceBase.eager_load_service_controllers
@@ -27,5 +17,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+	ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
+
