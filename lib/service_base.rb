0be948fa33a30def4922bd64c071132d0697f699
Rafał Bigaj <rafal.bigaj@gmail.com>
2014-09-16 16:29:55 +0200

    refs #13341 Watchdog monitoring service process
    # New process 'PF4XXX_Service.exe'
    # Process Management host info and restart support for many watchdogs on different hosts
    # Bootstrap and backbone cleanup

--- lib/service_base.rb
+++ lib/service_base.rb
@@ -40,13 +40,26 @@ module ServiceBase
 
 		def start_services(rabbit_config=nil, logger=nil)
 			ControllerManager.start(rabbit_config, logger)
+			true
 		rescue
 			if logger
 				logger.error "Error during service start"
 				logger.error "#{$!.class}: #{$!}"
 				logger.error $!.backtrace.join("\n")
 			end
-			raise
+			false
+		end
+
+		def stop_services(logger=nil)
+			ControllerManager.stop
+			true
+		rescue
+			if logger
+				logger.error "Error during service stop"
+				logger.error "#{$!.class}: #{$!}"
+				logger.error $!.backtrace.join("\n")
+			end
+			false
 		end
 
 		def services_running?
@@ -56,6 +69,14 @@ module ServiceBase
 		def restart_services(rabbit_config=nil, logger=nil)
 			ControllerManager.stop if ControllerManager.running?
 			ControllerManager.start(rabbit_config, logger)
+			true
+		rescue
+			if logger
+				logger.error "Error during service restart"
+				logger.error "#{$!.class}: #{$!}"
+				logger.error $!.backtrace.join("\n")
+			end
+			false
 		end
 
 		def eager_load_service_messages
