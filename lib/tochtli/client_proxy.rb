f29a0f2b76620ddc75d82563b57a0de8c1ab4d8d
Rafal Bigaj <rafal.bigaj@puzzleflow.com>
2015-05-29 11:08:48 +0200

    Redundant file removed

--- lib/tochtli/client_proxy.rb
+++
@@ -1,7 +0,0 @@
-module Tochtli
-  module ClientProxy
-    autoload :Base, 'tochtli/client_proxy/base'
-    autoload :Blocking, 'tochtli/client_proxy/blocking'
-    autoload :Async, 'tochtli/client_proxy/async'
-  end
-end
